16414ac0272f3b3b04d1d90fea52916cb1c4a3e1
YannRodriguez <52059549+YannRodriguez@users.noreply.github.com>
2022-11-20 15:31:47 +0100

    🐈‍⬛ apply black

--- model/vit.py
+++ model/vit.py
@@ -11,29 +11,41 @@ class ViT(nn.Module):
     Args:
         nn (_type_): _description_
     """
-    def __init__(self, *, image_size, patch_size, num_classes, dim, depth, heads, mlp_dim, channels=3):
+
+    def __init__(
+        self,
+        *,
+        image_size,
+        patch_size,
+        num_classes,
+        dim,
+        depth,
+        heads,
+        mlp_dim,
+        channels=3
+    ):
         super().__init__()
         image_height, image_width = image_size, image_size
         patch_height, patch_width = patch_size, patch_size
 
-        assert image_height % patch_height == 0 and image_width % patch_width == 0, 'Image dimensions must be divisible by the patch size.'
+        assert (
+            image_height % patch_height == 0 and image_width % patch_width == 0
+        ), "Image dimensions must be divisible by the patch size."
 
         num_patches = (image_height // patch_height) * (image_width // patch_width)
         patch_dim = channels * patch_height * patch_width
 
         self.patch_size = patch_size
 
-        self.pos_embedding = nn.Parameter(torch.randn(1, num_patches + 1, dim*depth))
-        self.patch_to_embedding = nn.Linear(patch_dim, dim*depth)
-        self.cls_token = nn.Parameter(torch.randn(1, 1, dim*depth))
-        self.transformer = nn.Transformer(dim*depth, depth, heads, mlp_dim)
+        self.pos_embedding = nn.Parameter(torch.randn(1, num_patches + 1, dim * depth))
+        self.patch_to_embedding = nn.Linear(patch_dim, dim * depth)
+        self.cls_token = nn.Parameter(torch.randn(1, 1, dim * depth))
+        self.transformer = nn.Transformer(dim * depth, depth, heads, mlp_dim)
 
         self.to_cls_token = nn.Identity()
 
         self.mlp_head = nn.Sequential(
-            nn.Linear(dim, mlp_dim),
-            nn.GELU(),
-            nn.Linear(mlp_dim, num_classes)
+            nn.Linear(dim, mlp_dim), nn.GELU(), nn.Linear(mlp_dim, num_classes)
         )
 
     def forward(self, img, mask=None):
@@ -48,7 +60,7 @@ class ViT(nn.Module):
         """
         p = self.patch_size
 
-        x = rearrange(img, 'b c (h p1) (w p2) -> b (h w) (p1 p2 c)', p1=p, p2=p)
+        x = rearrange(img, "b c (h p1) (w p2) -> b (h w) (p1 p2 c)", p1=p, p2=p)
         x = self.patch_to_embedding(x)
 
         cls_tokens = self.cls_token.expand(img.shape[0], -1, -1)
