c11e2a5a96cfac4ff8ca1a6960e43cefdb69dde6
YannRodriguez <52059549+YannRodriguez@users.noreply.github.com>
2022-11-20 15:30:22 +0100

    🐣 I believe in you

--- model/vit.py
+++ model/vit.py
@@ -5,237 +5,56 @@ from einops import rearrange, repeat
 from einops.layers.torch import Rearrange
 
 
-def pair(t):
-    """_summary_
-
-    Args:
-        t (_type_): _description_
-
-    Returns:
-        _type_: _description_
-    """
-    return t if isinstance(t, tuple) else (t, t)
-
-
-class PreNorm(nn.Module):
-    """_summary_
-
-    Args:
-        nn (_type_): _description_
-    """
-
-    def __init__(self, dim, fn):
-        super().__init__()
-        self.norm = nn.LayerNorm(dim)
-        self.fn = fn
-
-    def forward(self, x, **kwargs):
-        """_summary_
-
-        Args:
-            x (_type_): _description_
-
-        Returns:
-            _type_: _description_
-        """
-        return self.fn(self.norm(x), **kwargs)
-
-
-class FeedForward(nn.Module):
-    """_summary_
-
-    Args:
-        nn (_type_): _description_
-    """
-
-    def __init__(self, dim, hidden_dim, dropout=0.0):
-        super().__init__()
-        self.net = nn.Sequential(
-            nn.Linear(dim, hidden_dim),
-            nn.GELU(),
-            nn.Dropout(dropout),
-            nn.Linear(hidden_dim, dim),
-            nn.Dropout(dropout),
-        )
-
-    def forward(self, x):
-        """_summary_
-
-        Args:
-            x (_type_): _description_
-
-        Returns:
-            _type_: _description_
-        """
-        return self.net(x)
-
-
-class Attention(nn.Module):
-    """_summary_
-
-    Args:
-        nn (_type_): _description_
-    """
-
-    def __init__(self, dim, heads=8, dim_head=64, dropout=0.0):
-        super().__init__()
-        inner_dim = dim_head * heads
-        project_out = not (heads == 1 and dim_head == dim)
-
-        self.heads = heads
-        self.scale = dim_head ** -0.5
-
-        self.attend = nn.Softmax(dim=-1)
-        self.dropout = nn.Dropout(dropout)
-
-        self.to_qkv = nn.Linear(dim, inner_dim * 3, bias=False)
-
-        self.to_out = (
-            nn.Sequential(nn.Linear(inner_dim, dim), nn.Dropout(dropout))
-            if project_out
-            else nn.Identity()
-        )
-
-    def forward(self, x):
-        """_summary_
-
-        Args:
-            x (_type_): _description_
-
-        Returns:
-            _type_: _description_
-        """
-        qkv = self.to_qkv(x).chunk(3, dim=-1)
-        q, k, v = map(lambda t: rearrange(t, "b n (h d) -> b h n d", h=self.heads), qkv)
-
-        dots = torch.matmul(q, k.transpose(-1, -2)) * self.scale
-
-        attn = self.attend(dots)
-        attn = self.dropout(attn)
-
-        out = torch.matmul(attn, v)
-        out = rearrange(out, "b h n d -> b n (h d)")
-        return self.to_out(out)
-
-
-class Transformer(nn.Module):
-    """_summary_
-
-    Args:
-        nn (_type_): _description_
-    """
-
-    def __init__(self, dim, depth, heads, dim_head, mlp_dim, dropout=0.0):
-        super().__init__()
-        self.layers = nn.ModuleList([])
-        for _ in range(depth):
-            self.layers.append(
-                nn.ModuleList(
-                    [
-                        PreNorm(
-                            dim,
-                            Attention(
-                                dim, heads=heads, dim_head=dim_head, dropout=dropout
-                            ),
-                        ),
-                        PreNorm(dim, FeedForward(dim, mlp_dim, dropout=dropout)),
-                    ]
-                )
-            )
-
-    def forward(self, x):
-        """_summary_
-
-        Args:
-            x (_type_): _description_
-
-        Returns:
-            _type_: _description_
-        """
-        for attn, ff in self.layers:
-            x = attn(x) + x
-            x = ff(x) + x
-        return x
-
-
 class ViT(nn.Module):
     """_summary_
 
     Args:
         nn (_type_): _description_
     """
-
-    def __init__(
-        self,
-        *,
-        image_size,
-        patch_size,
-        num_classes,
-        dim,
-        depth,
-        heads,
-        mlp_dim,
-        pool="cls",
-        channels=3,
-        dim_head=64,
-        dropout=0.0,
-        emb_dropout=0.0
-    ):
+    def __init__(self, *, image_size, patch_size, num_classes, dim, depth, heads, mlp_dim, channels=3):
         super().__init__()
-        image_height, image_width = pair(image_size)
-        patch_height, patch_width = pair(patch_size)
+        image_height, image_width = image_size, image_size
+        patch_height, patch_width = patch_size, patch_size
 
-        assert (
-            image_height % patch_height == 0 and image_width % patch_width == 0
-        ), "Image dimensions must be divisible by the patch size."
+        assert image_height % patch_height == 0 and image_width % patch_width == 0, 'Image dimensions must be divisible by the patch size.'
 
         num_patches = (image_height // patch_height) * (image_width // patch_width)
         patch_dim = channels * patch_height * patch_width
-        assert pool in {
-            "cls",
-            "mean",
-        }, "pool type must be either cls (cls token) or mean (mean pooling)"
 
-        self.to_patch_embedding = nn.Sequential(
-            Rearrange(
-                "b c (h p1) (w p2) -> b (h w) (p1 p2 c)",
-                p1=patch_height,
-                p2=patch_width,
-            ),
-            nn.Linear(patch_dim, dim),
+        self.patch_size = patch_size
+
+        self.pos_embedding = nn.Parameter(torch.randn(1, num_patches + 1, dim*depth))
+        self.patch_to_embedding = nn.Linear(patch_dim, dim*depth)
+        self.cls_token = nn.Parameter(torch.randn(1, 1, dim*depth))
+        self.transformer = nn.Transformer(dim*depth, depth, heads, mlp_dim)
+
+        self.to_cls_token = nn.Identity()
+
+        self.mlp_head = nn.Sequential(
+            nn.Linear(dim, mlp_dim),
+            nn.GELU(),
+            nn.Linear(mlp_dim, num_classes)
         )
 
-        self.pos_embedding = nn.Parameter(torch.randn(1, num_patches + 1, dim))
-        self.cls_token = nn.Parameter(torch.randn(1, 1, dim))
-        self.dropout = nn.Dropout(emb_dropout)
-
-        self.transformer = Transformer(dim, depth, heads, dim_head, mlp_dim, dropout)
-
-        self.pool = pool
-        self.to_latent = nn.Identity()
-
-        self.mlp_head = nn.Sequential(nn.LayerNorm(dim), nn.Linear(dim, num_classes))
-
-    def forward(self, img):
+    def forward(self, img, mask=None):
         """_summary_
 
         Args:
             img (_type_): _description_
+            mask (_type_, optional): _description_. Defaults to None.
 
         Returns:
             _type_: _description_
         """
-        x = self.to_patch_embedding(img)
-        b, n, _ = x.shape
+        p = self.patch_size
 
-        cls_tokens = repeat(self.cls_token, "1 1 d -> b 1 d", b=b)
+        x = rearrange(img, 'b c (h p1) (w p2) -> b (h w) (p1 p2 c)', p1=p, p2=p)
+        x = self.patch_to_embedding(x)
+
+        cls_tokens = self.cls_token.expand(img.shape[0], -1, -1)
         x = torch.cat((cls_tokens, x), dim=1)
-        x += self.pos_embedding[:, : (n + 1)]
-        x = self.dropout(x)
+        x += self.pos_embedding
+        x = self.transformer(x, mask)
 
-        x = self.transformer(x)
-
-        x = x.mean(dim=1) if self.pool == "mean" else x[:, 0]
-
-        x = self.to_latent(x)
+        x = self.to_cls_token(x[:, 0])
         return self.mlp_head(x)
